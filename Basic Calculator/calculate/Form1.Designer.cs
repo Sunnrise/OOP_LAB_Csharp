a2455d2c11ebee39b9f7ffb07775cce09aa13bac
alperen güneş <Alperen GNE>
2024-03-21 18:20:07 +0300

    Basic calculator with c# forms

---
+++ Basic Calculator/calculate/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace calculate
+{
+    partial class Calculation
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Addition = new System.Windows.Forms.Button();
+            this.Substaction = new System.Windows.Forms.Button();
+            this.Multiplication = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "First Number";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(434, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Second Number";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Addition
+            // 
+            this.Addition.AccessibleDescription = "BtnAddition";
+            this.Addition.AccessibleName = "BtnAddition";
+            this.Addition.Location = new System.Drawing.Point(125, 239);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(118, 47);
+            this.Addition.TabIndex = 2;
+            this.Addition.Text = "Addition";
+            this.Addition.UseVisualStyleBackColor = true;
+            this.Addition.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Substaction
+            // 
+            this.Substaction.Location = new System.Drawing.Point(249, 239);
+            this.Substaction.Name = "Substaction";
+            this.Substaction.Size = new System.Drawing.Size(118, 47);
+            this.Substaction.TabIndex = 3;
+            this.Substaction.Text = "Substraction";
+            this.Substaction.UseVisualStyleBackColor = true;
+            this.Substaction.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Multiplication
+            // 
+            this.Multiplication.Location = new System.Drawing.Point(373, 239);
+            this.Multiplication.Name = "Multiplication";
+            this.Multiplication.Size = new System.Drawing.Size(118, 47);
+            this.Multiplication.TabIndex = 4;
+            this.Multiplication.Text = "Multiplication";
+            this.Multiplication.UseVisualStyleBackColor = true;
+            this.Multiplication.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(497, 239);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(118, 47);
+            this.Division.TabIndex = 5;
+            this.Division.Text = "Division";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "RESULT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please Type Two Number For Calculation";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiplication);
+            this.Controls.Add(this.Substaction);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button Substaction;
+        private System.Windows.Forms.Button Multiplication;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Basic Calculator/calculate/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculate
+{
+    public partial class Calculation : Form
+    {
+        public Calculation()
+        {
+            InitializeComponent();
+            Addition.MouseEnter += buttonOperation_MouseEnter;
+            Addition.MouseLeave += buttonOperation_MouseLeave;
+            Substaction.MouseEnter += buttonOperation_MouseEnter;
+            Substaction.MouseLeave += buttonOperation_MouseLeave;
+            Multiplication.MouseEnter += buttonOperation_MouseEnter;
+            Multiplication.MouseLeave += buttonOperation_MouseLeave;
+            Division.MouseEnter += buttonOperation_MouseEnter;
+            Division.MouseLeave += buttonOperation_MouseLeave;
+            textBox1.Click += textBox1_Enter;
+            textBox2.Click += textBox2_Enter;
+
+
+        }
+
+        private void CalculateResult(string operation)
+        {
+            try
+            {
+                double num1 = double.Parse(textBox1.Text);
+                double num2 = double.Parse(textBox2.Text);
+                double result = 0;
+
+                switch (operation)
+                {
+                    case "Addition":
+                        result = num1 + num2;
+                        break;
+                    case "Subtraction":
+                        result = num1 - num2;
+                        break;
+                    case "Multiplication":
+                        result = num1 * num2;
+                        break;
+                    case "Division":
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                            MessageBox.Show("Cannot divide by zero!");
+                        break;
+                }
+
+                label1.Text = result.ToString();
+                label1.BackColor = result > 0 ? Color.Green : Color.Red;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input. Please enter valid numbers.");
+            }
+        }
+
+
+
+        private void buttonOperation_MouseEnter(object sender, EventArgs e)
+        {
+            
+            ((Button)sender).BackColor = Color.White; // Arka plan rengini değiştirir
+            ((Button)sender).ForeColor = Color.Red; // Metin rengini değiştirir
+        }
+
+        private void buttonOperation_MouseLeave(object sender, EventArgs e)
+        {
+            ((Button)sender).ForeColor = SystemColors.ControlText;
+           
+
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "First Number")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = SystemColors.ControlText; // Varsayılan metin rengi
+            }
+        }
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Second Number")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = SystemColors.ControlText; // Varsayılan metin rengi
+            }
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "Enter your name";
+                textBox1.ForeColor = SystemColors.GrayText; // Gri renkli metin
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalculateResult("Addition");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CalculateResult("Subtraction");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CalculateResult("Multiplication");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CalculateResult("Division");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
