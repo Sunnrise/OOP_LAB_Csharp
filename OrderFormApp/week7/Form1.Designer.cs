542a1f7625079ac6d02fa2fa3c280251b3b937d8
alperen güneş <Alperen GNE>
2024-05-16 19:06:36 +0300

    Order form app inclass activity

---
+++ OrderFormApp/week7/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace week7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCreateOrder = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.listBoxCity = new System.Windows.Forms.ListBox();
+            this.listBoxDistrict = new System.Windows.Forms.ListBox();
+            this.listBoxBrand = new System.Windows.Forms.ListBox();
+            this.listBoxOrders = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnCreateOrder
+            // 
+            this.BtnCreateOrder.Location = new System.Drawing.Point(43, 355);
+            this.BtnCreateOrder.Name = "BtnCreateOrder";
+            this.BtnCreateOrder.Size = new System.Drawing.Size(133, 37);
+            this.BtnCreateOrder.TabIndex = 0;
+            this.BtnCreateOrder.Text = "CREATE ORDER";
+            this.BtnCreateOrder.UseVisualStyleBackColor = true;
+            this.BtnCreateOrder.Click += new System.EventHandler(this.BtnCreateOrder_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(43, 41);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(120, 22);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(43, 103);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(120, 22);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // listBoxCity
+            // 
+            this.listBoxCity.FormattingEnabled = true;
+            this.listBoxCity.ItemHeight = 16;
+            this.listBoxCity.Location = new System.Drawing.Point(455, 41);
+            this.listBoxCity.Name = "listBoxCity";
+            this.listBoxCity.Size = new System.Drawing.Size(120, 84);
+            this.listBoxCity.TabIndex = 3;
+            this.listBoxCity.SelectedIndexChanged += new System.EventHandler(this.listBoxCity_SelectedIndexChanged);
+            // 
+            // listBoxDistrict
+            // 
+            this.listBoxDistrict.FormattingEnabled = true;
+            this.listBoxDistrict.ItemHeight = 16;
+            this.listBoxDistrict.Location = new System.Drawing.Point(455, 149);
+            this.listBoxDistrict.Name = "listBoxDistrict";
+            this.listBoxDistrict.Size = new System.Drawing.Size(120, 84);
+            this.listBoxDistrict.TabIndex = 4;
+            // 
+            // listBoxBrand
+            // 
+            this.listBoxBrand.FormattingEnabled = true;
+            this.listBoxBrand.ItemHeight = 16;
+            this.listBoxBrand.Location = new System.Drawing.Point(43, 149);
+            this.listBoxBrand.Name = "listBoxBrand";
+            this.listBoxBrand.Size = new System.Drawing.Size(120, 84);
+            this.listBoxBrand.TabIndex = 5;
+            // 
+            // listBoxOrders
+            // 
+            this.listBoxOrders.FormattingEnabled = true;
+            this.listBoxOrders.ItemHeight = 16;
+            this.listBoxOrders.Location = new System.Drawing.Point(455, 260);
+            this.listBoxOrders.Name = "listBoxOrders";
+            this.listBoxOrders.Size = new System.Drawing.Size(120, 84);
+            this.listBoxOrders.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton8);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Location = new System.Drawing.Point(43, 239);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(229, 93);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(107, 50);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(42, 20);
+            this.radioButton7.TabIndex = 18;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "42";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(164, 50);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(42, 20);
+            this.radioButton8.TabIndex = 19;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "43";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(42, 20);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "36";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(107, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 20);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "38";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(57, 50);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(42, 20);
+            this.radioButton6.TabIndex = 17;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "41";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 50);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(42, 20);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "40";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(164, 21);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(42, 20);
+            this.radioButton4.TabIndex = 15;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "39";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(57, 21);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(42, 20);
+            this.radioButton5.TabIndex = 16;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "37";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Phone Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(462, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(462, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "District";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(462, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Order";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBoxOrders);
+            this.Controls.Add(this.listBoxBrand);
+            this.Controls.Add(this.listBoxDistrict);
+            this.Controls.Add(this.listBoxCity);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.BtnCreateOrder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCreateOrder;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.ListBox listBoxCity;
+        private System.Windows.Forms.ListBox listBoxDistrict;
+        private System.Windows.Forms.ListBox listBoxBrand;
+        private System.Windows.Forms.ListBox listBoxOrders;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ OrderFormApp/week7/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreateOrder_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Please enter a username.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsername.Focus(); // Set focus back to the username TextBox
+                return; // Exit the event handler
+            }
+            if (txtPhone.Text == "")
+            {
+                MessageBox.Show("Please enter a phone number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPhone.Focus(); // Set focus back to the phone number TextBox
+                return; // Exit the event handler
+            }
+            if (listBoxBrand.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a brand.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                listBoxBrand.Focus(); // Set focus back to the brand ListBox
+                return; // Exit the event handler
+            }
+            if (listBoxCity.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a city.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                listBoxCity.Focus(); // Set focus back to the city ListBox
+                return; // Exit the event handler
+            }
+            if (listBoxDistrict.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a district.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                listBoxDistrict.Focus(); // Set focus back to the district ListBox
+                return; // Exit the event handler
+            }
+            if (GetSelectedRadioButton(groupBox1) == null)
+            {
+                MessageBox.Show("Please select a size.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Exit the event handler
+            }
+            // Kullanımı
+            RadioButton selectedRadioButton= GetSelectedRadioButton(groupBox1);
+
+            
+            // Get user inputs
+            string username = txtUsername.Text;
+            string size = selectedRadioButton.Text;
+            string phone = txtPhone.Text;
+            string brand = listBoxBrand.SelectedItem.ToString();
+            
+            //string size = listBoxSize.SelectedItem.ToString(); // Changed from CheckBox to ListBox
+            string city = listBoxCity.SelectedItem.ToString();
+            string district = listBoxDistrict.SelectedItem.ToString();
+            string order = $"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")} - {username}, {phone}, {brand} , Size: {size}, City: {city}, District: {district}";
+
+            // Add order to ListBox
+            listBoxOrders.Items.Add(order);
+
+            string phoneNumber = txtPhone.Text;
+
+            if (IsValidPhoneNumber(phoneNumber))
+            {
+                MessageBox.Show("Valid phone number.");
+            }
+            else
+            {
+                MessageBox.Show("Invalid phone number. Please enter a valid phone number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPhone.Focus(); // Set focus back to the phone number TextBox
+                txtPhone.SelectAll(); // Select all text in the TextBox for easy correction
+            }
+            
+
+            if (!IsValidPhoneNumber(phoneNumber))
+            {
+                MessageBox.Show("Invalid phone number. Please enter a valid phone number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            label1.Text = order;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize ListBox items
+            listBoxBrand.Items.AddRange(new string[] { "Adidas", "Nike", "Puma" });
+             // Example shoe sizes
+            listBoxCity.Items.AddRange(new string[] { "Eskişehir", "Ankara" });
+            // Populate districts based on selected city
+            PopulateDistricts("Eskişehir");
+        }
+
+        
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Populate districts based on selected city
+            string selectedCity = listBoxCity.SelectedItem.ToString();
+            PopulateDistricts(selectedCity);
+        }
+        private void PopulateDistricts(string city)
+        {
+            listBoxDistrict.Items.Clear();
+            if (city == "Eskişehir")
+            {
+                listBoxDistrict.Items.AddRange(new string[] { "Sivrihisar", "Odunpazarı", "Tepebaşı" });
+            }
+            else if (city == "Ankara")
+            {
+                listBoxDistrict.Items.AddRange(new string[] { "Mamak", "Çankaya", "Polatlı" });
+            }
+           
+        }
+        static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // Regex pattern to match common phone number formats
+            string pattern = @"^(\+?90|0)?(\d{3})?(\d{3})(\d{2})(\d{2})$";
+            // Adjust this pattern based on your requirements
+
+            // Create a Regex object
+            Regex regex = new Regex(pattern);
+
+            // Use the IsMatch method to check if the phone number matches the pattern
+            return regex.IsMatch(phoneNumber);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private RadioButton GetSelectedRadioButton(GroupBox groupBox)
+        {
+            foreach (Control control in groupBox.Controls)
+            {
+                if (control is RadioButton radioButton && radioButton.Checked)
+                {
+                    return radioButton;
+                }
+            }
+            return null; // Seçili bir radyo butonu bulunamazsa null döner
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtUsername_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = null;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
